1c4603645bdb29d071809c237b1a05002b52781c
maexpe <matheusexpedito58@gmail.com>
2023-10-06 15:16:54 -0300

    feat: Adição de model AlunoModel para aluno

---
+++ OperacoesAlunoTurma/OperacoesAlunoTurma/Models/AlunoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace OperacoesAlunoTurma.Models
+{
+    public class AlunoModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Usuario { get; set; }
+        public string Senha { get; set; }
+    }
+}
