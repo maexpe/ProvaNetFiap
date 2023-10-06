5228185e4a275bf3a3d683f2ae0302eb94a74c2f
maexpe <matheusexpedito58@gmail.com>
2023-10-06 15:18:57 -0300

    feat: Adição de model TurmaModel para turma

---
+++ OperacoesAlunoTurma/OperacoesAlunoTurma/Models/TurmaModel.cs
@@ -0,0 +1,10 @@
+﻿namespace OperacoesAlunoTurma.Models
+{
+    public class TurmaModel
+    {
+        public int Id { get; set; }
+        public int CursoId { get; set; }
+        public string Turma { get; set; }
+        public int Ano { get; set; }
+    }
+}
