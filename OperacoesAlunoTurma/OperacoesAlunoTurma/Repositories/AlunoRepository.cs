15e4708901b076a34fb80e8166b32f0693efd400
maexpe <matheusexpedito58@gmail.com>
2023-10-06 15:52:14 -0300

    feat: Adição de AlunoRepository com operações CRUD básicas

---
+++ OperacoesAlunoTurma/OperacoesAlunoTurma/Repositories/AlunoRepository.cs
@@ -0,0 +1,47 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using OperacoesAlunoTurma.Models;
+
+namespace OperacoesAlunoTurma.Repositories
+{
+    public class AlunoRepository
+    {
+        private readonly string _connectionString;
+
+        public AlunoRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IEnumerable<AlunoModel> GetAll()
+        {
+            using var connection = new SqlConnection(_connectionString);
+            connection.Open();
+            return connection.Query<AlunoModel>("select * from aluno");
+        }
+
+        public void Add(AlunoModel aluno)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            connection.Open();
+            var query = "insert into aluno(nome, usuario, senha) values (@nome, @usuario, @senha)";
+            connection.Execute(query, aluno);
+        }
+
+        public void Update(AlunoModel aluno)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            connection.Open();
+            var query = "update aluno set nome = @nome, usuario = @usuario, senha = @senha where id = @id";
+            connection.Execute(query, aluno);
+        }
+
+        public void Delete(int id)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            connection.Open();
+            var query = "delete from aluno where id = @id";
+            connection.Execute(query, new { Id = id });
+        }
+    }
+}
