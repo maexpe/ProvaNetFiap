a95db6dfe27703d2bc8400fcf458f7468dea1111
maexpe <matheusexpedito58@gmail.com>
2023-10-06 19:15:40 -0300

    feat: Adição de interface IAlunoService

---
+++ OperacoesAlunoTurma/OperacoesAlunoTurma/Interfaces/IAlunoService.cs
@@ -0,0 +1,13 @@
+﻿using OperacoesAlunoTurma.Models;
+
+namespace OperacoesAlunoTurma.Interfaces
+{
+    public interface IAlunoService
+    {
+        bool SenhaForte(string senha);
+        IEnumerable<AlunoModel> GetAll();
+        void Add(AlunoModel aluno);
+        void Update(AlunoModel aluno);
+        void Delete(int id);
+    }
+}
