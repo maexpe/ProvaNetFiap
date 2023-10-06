20c9e1c249007818099d054f6b190b7a13b5e0b1
maexpe <matheusexpedito58@gmail.com>
2023-10-06 20:53:13 -0300

    feat: Adição de interface ITurmaService

---
+++ OperacoesAlunoTurma/OperacoesAlunoTurma/Interfaces/ITurmaService.cs
@@ -0,0 +1,13 @@
+﻿using OperacoesAlunoTurma.Models;
+
+namespace OperacoesAlunoTurma.Interfaces
+{
+    public interface ITurmaService
+    {
+        bool TurmaNomeUnique(string turmaNome);
+        IEnumerable<TurmaModel> GetAll();
+        void Add(TurmaModel turma);
+        void Update(TurmaModel turma);
+        void Delete(int id);
+    }
+}
